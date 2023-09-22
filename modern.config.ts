8b4c4a637f1e192215f23a764bcf9b411ac97729
chenjiahan <chenjiahan.jait@bytedance.com>
2023-09-22 14:54:51 +0800

    chore: reproduce

--- modern.config.ts
+++ modern.config.ts
@@ -2,5 +2,10 @@ import { moduleTools, defineConfig } from '@modern-js/module-tools';
 
 export default defineConfig({
   plugins: [moduleTools()],
-  buildPreset: 'npm-library',
+  buildConfig: {
+    buildType: 'bundleless',
+    style: {
+      autoModules: /\.css$/,
+    },
+  },
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import styles from './index.css';
+
 export default function () {
+  console.log(styles);
   return 'hello world';
 }
